11da2c9b1c6a2606f53302c47718cec698c035c7
Your Name <nguyenphuonganh26042005@gmail.com>
2025-06-03 09:03:04 +0700

    Add main.py file

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-friends=5
-#friends= friends + 1
-#friends += 1  # Incrementing the friends variable by 1
-#friends=friends -2
-#friends=friends * 3 
-friends *=3
-print(friends)
+print("Hello GitHub!")
